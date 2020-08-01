f4fdd99760ae6f85d476efac4bc11ac5d9176942
Louis Jacquelin <jacquelinlouis@gmail.com>
2020-08-01 08:57:53 +0200

    Rename fragment first to list

--- app/src/main/java/com/jac/mynote/view/ListFragment.kt
+++ app/src/main/java/com/jac/mynote/view/ListFragment.kt
@@ -5,15 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.jac.mynote.R
 import com.jac.mynote.viewmodel.MyNoteViewModel
 
@@ -29,7 +26,7 @@ class ListFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
