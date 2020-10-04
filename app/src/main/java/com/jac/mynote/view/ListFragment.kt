24ddde6834eec9c6ed539a0faffd1920b95acd05
Louis Jacquelin <jacquelinlouis@gmail.com>
2020-10-04 11:03:43 +0200

    Remove unused imports

--- app/src/main/java/com/jac/mynote/view/ListFragment.kt
+++ app/src/main/java/com/jac/mynote/view/ListFragment.kt
@@ -1,24 +1,20 @@
 package com.jac.mynote.view
 
 import android.os.Bundle
-import android.view.*
-import android.widget.AdapterView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.PopupMenu
-import androidx.annotation.NonNull
-import androidx.annotation.Nullable
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jac.mynote.R
 import com.jac.mynote.model.Note
-import com.jac.mynote.model.SingleContentNote
 import com.jac.mynote.viewmodel.MyNoteViewModel
 
 /**
