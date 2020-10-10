e943362dca67ba71f270092d4774cc63dbaf5a79
Louis Jacquelin <jacquelinlouis@gmail.com>
2020-10-10 16:48:15 +0200

    Comment NoteEntity

--- app/src/main/java/com/jac/mynote/data/NoteEntity.kt
+++ app/src/main/java/com/jac/mynote/data/NoteEntity.kt
@@ -4,12 +4,24 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/** Note entity representing a unique note in database. */
 @Entity()
 data class NoteEntity (
+    /** Generated primary identifier for this note, which never change.  */
     @PrimaryKey(autoGenerate = true) val uid: Int,
+    /** Title of the note. */
     @ColumnInfo(name = "title") var title: String,
+    /** Type of the note, which can be a text, a password, or something else like a list. */
     @ColumnInfo(name = "type") var type: String,
+    /** Content of the note, as a raw string. */
     @ColumnInfo(name = "content") var content: String
 ) {
+    /**
+     * Note entity constructor.
+     * Identifier is generated, default value 0 is replaced by last created note's identifier + 1.
+     * @param title note's title.
+     * @param type note's type.
+     * @param content note's content as a raw string.
+     */
     constructor(title: String, type: String, content: String) : this(0, title, type, content)
 }
