ce80f6da94750a85ac7d95e97499ed89861eded9
Louis Jacquelin <jacquelinlouis@gmail.com>
2020-10-10 16:56:17 +0200

    Comment NotesDao

--- app/src/main/java/com/jac/mynote/data/NotesDao.kt
+++ app/src/main/java/com/jac/mynote/data/NotesDao.kt
@@ -4,20 +4,48 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.jac.mynote.data.NoteEntity
 
+/** Notes data object, used to get, add, update or delete notes. */
 @Dao
 interface NotesDao {
+    /**
+     * Getter to retrieve all {@link NoteEntity} at once.
+     *
+     * @return the whole list in a {@link LiveData} object.
+     * Each note add, update or delete of a component of this list will be notified to listeners.
+     */
     @Query("SELECT * FROM NoteEntity")
     fun getAll(): LiveData<List<NoteEntity>>
 
+    /**
+     * Creator for one or more {@link NoteEntity}.
+     *
+     * @param noteEntity variable number of {@link NoteEntity} to insert in the database.
+     */
     @Insert
     suspend fun insertNoteEntities(vararg noteEntity: NoteEntity)
 
+    /**
+     * Update the given {@link NoteEntity}, retrieving the instance to update by its identifier.
+     *
+     * @param noteEntity the {@link NoteEntity} to update.
+     */
     @Update
     suspend fun updateNoteEntity(noteEntity: NoteEntity)
 
+    /**
+     * Delete the given {@link NoteEntity} instance.
+     * Check all it members before deletion.
+     *
+     * @param noteEntity the {@link NoteEntity} to delete.
+     */
     @Delete
     suspend fun deleteNoteEntity(noteEntity: NoteEntity)
 
+    /**
+     * Delete the given {@link NoteEntity} instance.
+     *
+     * @param id the {@link NoteEntity} to delete identifier.
+     */
     @Query("DELETE FROM NoteEntity WHERE uid = :id")
     suspend fun deleteNoteEntity(id: Int);
 }
