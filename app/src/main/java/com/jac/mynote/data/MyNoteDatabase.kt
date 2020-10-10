c69eb70f023f0ce824022befaa5799c7b7faa995
Louis Jacquelin <jacquelinlouis@gmail.com>
2020-10-10 16:43:19 +0200

    Comment MyNoteDatabase

--- app/src/main/java/com/jac/mynote/data/MyNoteDatabase.kt
+++ app/src/main/java/com/jac/mynote/data/MyNoteDatabase.kt
@@ -6,11 +6,27 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.jac.mynote.MainActivity
 
+/**
+ * Main database access class.
+ */
 @Database(entities = [NoteEntity::class], version = 1)
 abstract class MyNoteDatabase: RoomDatabase() {
+
     companion object {
+
+        /** Database name, for internal use only. */
         private const val DATABASE_NAME = "MyNoteDatabase"
+
+        /** Database instance, unique for the whole application. */
         private var myNoteDatabaseInstance: MyNoteDatabase? = null
+
+        /**
+         * Unique entry point to retrieve the database instance.
+         *
+         * @param context the Android context to build database with on first call.
+         *
+         * @return existing database instance or a new one on first call.
+         */
         fun getInstance(context: Context): MyNoteDatabase {
             if (myNoteDatabaseInstance == null) {
                 myNoteDatabaseInstance = Room.databaseBuilder(context, MyNoteDatabase::class.java, DATABASE_NAME).build()
@@ -19,5 +35,10 @@ abstract class MyNoteDatabase: RoomDatabase() {
         }
     }
 
+    /**
+     * Getter for notes data object instance.
+     *
+     * @return the note data object used to get, add, remove and update notes.
+     */
     abstract fun getNotesDao(): NotesDao
 }
