0fc7d5b5e3b359edb9cece0c75e5c7da02f05bdb
Louis Jacquelin <jacquelinlouis@gmail.com>
2021-05-05 18:39:38 +0200

    Move notes list to its own feature

--- app/src/main/java/com/jac/note/feature/noteslist/NotesListFragment.kt
+++ app/src/main/java/com/jac/note/feature/noteslist/NotesListFragment.kt
@@ -1,11 +1,10 @@
-package com.jac.note.view
+package com.jac.note.feature.noteslist
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
-import androidx.activity.OnBackPressedCallback
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -16,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jac.note.R
 import com.jac.note.model.Note
+import com.jac.note.view.NotesAdapter
 import com.jac.note.viewmodel.MyNoteViewModel
 
 /**
